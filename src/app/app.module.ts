50fdb01f0985f9bc8e7dec6042967b40e5f53c4b
Edwin Andres Tapia Lasso <e.tapia@uniandes.edu.co>
2023-03-12 07:25:59 -0500

    Maquetacion Ux Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserModule } from './user/user.module';
+import {MatCardModule} from '@angular/material/card';
+import { AlarmasModule } from './alarmas/alarmas.module';
+import { ToastrModule } from "ngx-toastr";
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+
+
+
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    UserModule,
+    MatCardModule,
+    AlarmasModule,
+    ToastrModule.forRoot({
+      timeOut: 1500,
+      positionClass: "toast-bottom-right",
+      preventDuplicates: true,
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
